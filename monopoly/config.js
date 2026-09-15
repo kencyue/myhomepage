e9afdfc1dc439805dadcc308f4661bd7a430a8ae
kencyue <jingsyuan@gmail.com>
2026-09-15 22:48:56 +0800

    refactor(monopoly): extract Firebase config

---
+++ monopoly/config.js
@@ -0,0 +1,4 @@
+// Firebase 網頁設定不是私密金鑰；存取安全性仍須由 Firestore Security Rules 控制。
+export const appId = "monoploy-c3855";
+export const firebaseConfig = {"apiKey":"AIzaSyCUNXRAPewJkvvMqkRU4aTPY1nb268UBso","authDomain":"monoploy-c3855.firebaseapp.com","projectId":"monoploy-c3855","storageBucket":"monoploy-c3855.firebasestorage.app","messagingSenderId":"452296239400","appId":"1:452296239400:web:2b15915e5b79005771081d","measurementId":"G-84MF1YMSED"};
+export const initialAuthToken = null;
